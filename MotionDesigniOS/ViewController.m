70a9aa71fdfe8c61b217d4622573b900409febe6
kavanbrandon <kavan.brandon@gmail.com>
2015-10-06 12:31:20 -0400

    Add UIAlertView Animations

--- MotionDesigniOS/ViewController.m
+++ MotionDesigniOS/ViewController.m
@@ -13,6 +13,7 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIView *designView;
 @property (weak, nonatomic) IBOutlet UIView *overlayView;
+- (IBAction)runButton:(id)sender;
 
 @end
 
@@ -24,17 +25,21 @@
 //    //Do any additional setup after loading the view, typically from a nib.
     self.designView.alpha = 0.0;
     self.overlayView.alpha = 0.0;
+}
+
+- (IBAction)runButton:(id)sender {
     
     [self setOverlayViewFrame];
+    [self setUIAlertViewBounce];
     
-//    [self basicScaleAnimation];
-//    [self animateWithConcat];
-//    [self animateWithDampingSpring];
+    //    [self basicScaleAnimation];
+    //    [self animateWithConcat];
+    //    [self animateWithDampingSpring];
     
-//    [self animateScaleWithJNWSpringAnimation];
-//    [self animateRotationWithJNWSpringAnimation];
-//    [self animateDampnessAndStiffnessWithJNWSpringAnimation];
-//    [self animateScaleAndRotationJNWSpringAnimation];
+    //    [self animateScaleWithJNWSpringAnimation];
+    //    [self animateRotationWithJNWSpringAnimation];
+    //    [self animateDampnessAndStiffnessWithJNWSpringAnimation];
+    //    [self animateScaleAndRotationJNWSpringAnimation];
 }
 
 -(void)basicScaleAnimation {
@@ -155,17 +160,232 @@
     self.designView.transform = CGAffineTransformRotate(self.designView.transform, M_PI);
 }
 
--(void)setOverlayViewFrame{
+-(void)setOverlayViewFrame {
     CGRect screenRect = [[UIScreen mainScreen] bounds];
-    
+    self.overlayView.translatesAutoresizingMaskIntoConstraints = YES;
     [self.overlayView setFrame:screenRect];
-    self.overlayView.backgroundColor = [UIColor blackColor];
-    self.overlayView.alpha = 1.0f;
+    self.overlayView.backgroundColor = [UIColor lightGrayColor];
+}
+
+-(void)setStandardUIAlertView {
+    CGFloat alertDimension = 250;
+    CGRect alertViewFrame = CGRectMake(self.overlayView.bounds.size.width/2 - alertDimension/2, self.overlayView.bounds.size.height/2 - alertDimension/2, alertDimension, alertDimension);
+    UIView *alertView = [[UIView alloc] initWithFrame:alertViewFrame];
+    alertView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"alert_box"]];
+    alertView.alpha = 0.0f;
+    alertView.transform = CGAffineTransformMakeScale(1.2, 1.2);
+    alertView.layer.cornerRadius = 10;
+    alertView.layer.shadowColor = [UIColor blackColor].CGColor;
+    alertView.layer.shadowOffset = CGSizeMake(0, 5);
+    alertView.layer.shadowOpacity = 0.3f;
+    alertView.layer.shadowRadius = 10.0f;
+    [self.view addSubview:alertView];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
+        
+        // Fade in the grey overlay
+        [UIView animateWithDuration:.3 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            self.overlayView.alpha = 0.2f;
+        } completion:NULL];
+        
+        // Fade in the alert view
+        [UIView animateWithDuration:.3 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            alertView.alpha = 1.0f;
+        } completion:NULL];
+        
+        // Scale-animate in the alert view
+        JNWSpringAnimation *scale = [JNWSpringAnimation animationWithKeyPath:@"transform.scale"];
+        scale.damping = 14;
+        scale.stiffness = 14;
+        scale.mass = 1;
+        scale.fromValue = @(1.2);
+        scale.toValue = @(1.0);
+        
+        [alertView.layer addAnimation:scale forKey:scale.keyPath];
+        alertView.transform = CGAffineTransformMakeScale(1.0, 1.0);
+        
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
+        
+        // Fade out the grey overlay
+        [UIView animateWithDuration:.15 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            self.overlayView.alpha = 0.0f;
+            alertView.alpha = 0.0f;
+        } completion:NULL];
+        
+        // Scale-animate in the alert view
+        JNWSpringAnimation *scaleAnimation = [JNWSpringAnimation animationWithKeyPath:@"transform.scale"];
+        scaleAnimation.damping = 11;
+        scaleAnimation.stiffness = 11;
+        scaleAnimation.mass = 1;
+        scaleAnimation.fromValue = @(1.0);
+        scaleAnimation.toValue = @(0.7);
+        
+        [alertView.layer addAnimation:scaleAnimation forKey:scaleAnimation.keyPath];
+        alertView.transform = CGAffineTransformMakeScale(0.7, 0.7);
+        
+    });
+}
+
+-(void)setUIAlertViewFromBottom {
+    CGFloat alertDimension = 250;
+    CGRect alertViewFrame = CGRectMake(self.overlayView.bounds.size.width/2 - alertDimension/2, self.overlayView.bounds.size.height/2 - alertDimension/2, alertDimension, alertDimension);
+    UIView *alertView = [[UIView alloc] initWithFrame:alertViewFrame];
+    alertView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"alert_box"]];
+    alertView.alpha = 0.0f;
+    CGAffineTransform viewTransform = CGAffineTransformConcat(CGAffineTransformMakeScale(.25, .25),
+                                                              CGAffineTransformMakeTranslation(0, 600));
+    alertView.transform = viewTransform;
+    alertView.layer.cornerRadius = 10;
+    alertView.layer.shadowColor = [UIColor blackColor].CGColor;
+    alertView.layer.shadowOffset = CGSizeMake(0, 5);
+    alertView.layer.shadowOpacity = 0.3f;
+    alertView.layer.shadowRadius = 10.0f;
+    [self.view addSubview:alertView];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
+        
+        // Fade the overlay and alert view together
+        [UIView animateWithDuration:.3 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            self.overlayView.alpha = 0.2f;
+            alertView.alpha = 1.0f;
+        } completion:NULL];
+        
+        // Animate the alert’s scale from .25 up to 1.0
+        JNWSpringAnimation *scale = [JNWSpringAnimation animationWithKeyPath:@"transform.scale"];
+        scale.damping = 12;
+        scale.stiffness = 12;
+        scale.mass = 1;
+        scale.fromValue = @(.25);
+        scale.toValue = @(1.0);
+        
+        [alertView.layer addAnimation:scale forKey:scale.keyPath];
+        alertView.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.0, 1.0);
+        
+        // Animate the position from the starting Y position of 600 back up to 0
+        // which puts it back at the original position
+        JNWSpringAnimation *translate = [JNWSpringAnimation animationWithKeyPath:@"transform.translation.y"];
+        translate.damping = 15;
+        translate.stiffness = 15;
+        translate.mass = 1;
+        translate.fromValue = @(600);
+        translate.toValue = @(0);
+        
+        [alertView.layer addAnimation:translate forKey:translate.keyPath];
+        alertView.transform = CGAffineTransformTranslate(alertView.transform, 0, 0);
+        
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 3 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
+
+        
+        [UIView animateWithDuration:.2 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            self.overlayView.alpha = 0.0f;
+            alertView.alpha = 0.0f;
+        } completion:NULL];
+        
+        JNWSpringAnimation *scale = [JNWSpringAnimation animationWithKeyPath:@"transform.scale"];
+        scale.damping = 17;
+        scale.stiffness = 17;
+        scale.mass = 1;
+        scale.fromValue = @(1.0);
+        scale.toValue = @(0.5);
+        
+        [alertView.layer addAnimation:scale forKey:scale.keyPath];
+        alertView.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.5, 0.5);
+        
+        JNWSpringAnimation *translate = [JNWSpringAnimation animationWithKeyPath:@"transform.translation.y"];
+        translate.damping = 4;
+        translate.stiffness = 4;
+        translate.mass = 1;
+        translate.fromValue = @(0);
+        translate.toValue = @(600);
+        
+        [alertView.layer addAnimation:translate forKey:translate.keyPath];
+        alertView.transform = CGAffineTransformTranslate(alertView.transform, 0, 600);
+    });
+}
+
+-(void)setUIAlertViewBounce {
+    CGFloat alertDimension = 250;
+    CGRect alertViewFrame = CGRectMake(self.overlayView.bounds.size.width/2 - alertDimension/2, self.overlayView.bounds.size.height/2 - alertDimension/2, alertDimension, alertDimension);
+    UIView *alertView = [[UIView alloc] initWithFrame:alertViewFrame];
+    alertView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"alert_box"]];
+    alertView.alpha = 0.0f;
+    alertView.transform = CGAffineTransformMakeScale(0, 0);
+    alertView.layer.cornerRadius = 10;
+    alertView.layer.shadowColor = [UIColor blackColor].CGColor;
+    alertView.layer.shadowOffset = CGSizeMake(0, 5);
+    alertView.layer.shadowOpacity = 0.3f;
+    alertView.layer.shadowRadius = 10.0f;
+    [self.view addSubview:alertView];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
+        
+        [UIView animateWithDuration:.2 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            self.overlayView.alpha = 0.2f;
+            alertView.alpha = 1.0f;
+        } completion:NULL];
+        
+        JNWSpringAnimation *scale = [JNWSpringAnimation animationWithKeyPath:@"transform.scale"];
+        scale.damping = 32;
+        scale.stiffness = 450;
+        scale.mass = 2.4;
+        scale.fromValue = @(0);
+        scale.toValue = @(1.0);
+        
+        [alertView.layer addAnimation:scale forKey:scale.keyPath];
+        alertView.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.0, 1.0);
+    });
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 4 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
+        
+        // Fade out the grey overlay
+        [UIView animateWithDuration:.2 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            self.overlayView.alpha = 0.0f;
+            alertView.alpha = 0.0f;
+        } completion:NULL];
+        
+        JNWSpringAnimation *scale = [JNWSpringAnimation animationWithKeyPath:@"transform.scale"];
+        scale.damping = 28;
+        scale.stiffness = 28;
+        scale.mass = 1;
+        scale.fromValue = @(1);
+        scale.toValue = @(0);
+        
+        [alertView.layer addAnimation:scale forKey:scale.keyPath];
+        alertView.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0, 0);
+        
+    });
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
